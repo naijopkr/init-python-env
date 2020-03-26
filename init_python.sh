eda8a4294421ee72a39f60ec0fc6f4489188cc03
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-03-26 15:27:30 -0500

    initial commit

---
+++ init_python.sh
@@ -0,0 +1,12 @@
+python3 -m venv venv
+source venv/bin/activate
+
+echo "venv/
+__pycache__/
+build/
+dist/
+.vscode/
+*.egg-info/" > .gitignore
+
+echo "if __name__ == '__main__':
+    print('Hello world')" > __main__.py
